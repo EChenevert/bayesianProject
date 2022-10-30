aa213e72dbfeac1f5c9bfd06ac25aa6a8facaf5b
Chenevert, Etienne A <etachen@iu.edu>
2022-10-30 18:09:55 -0400

    beginning of exploratort

--- bayesianTest_pymc3.py
+++ bayesianTest_pymc3.py
@@ -38,7 +38,8 @@ plt.title('KDE Density Plot of Accretion by Marsh Type')
 plt.show()
 
 # Investigate Accretion Percentiles
-from scipy.stats import percentileofscore
+from scipy.stats import percentileofscore, stats
+
 df['Percentile'] = df[outcome].apply(lambda x: percentileofscore(df[outcome], x))
 plt.figure()
 plt.plot(df[outcome], df['Percentile'], 'o')
@@ -46,6 +47,51 @@ plt.xlabel('Accretion Rate (mm/yr)'); plt.ylabel('Percentile'); plt.title('Accre
 plt.show()
 print('Min 50th Percentile Score: ', np.min(df.loc[df['Percentile'] > 50, outcome]))  # ~12 mm/yr
 print('Min 90th Percentile Score: ', np.min(df.loc[df['Percentile'] > 90, outcome]))  # ~20 mm/yr
+
+# Do the cool pairplot
+
+# Implement the Bayesian Linear Regression
+# Define Marsh Datasets
+marshDic = {}
+fresh = df[df['Community'] == 'Freshwater']
+saline = df[df['Community'] == 'Saline']
+intermediate = df[df['Community'] == 'Intermediate']
+Brackish = df[df['Community'] == 'Brackish']
+
+RSLR = df['RSLR (mm/yr)']
+dfwhole = df.drop(['Community', 'RSLR (mm/yr)', 'Percentile'], axis=1)
+df_vars = list(dfwhole.columns.values)
+
+# rename variables
+dfwhole = dfwhole.rename(columns={'Accretion Rate (mm/yr)': 'AccretionRate',
+                                  'avg_flooding (ft)': 'flood_depth',
+                                  '90%thUpper_flooding (ft)': 'Upperflood_depth',
+                                  '10%thLower_flooding (ft)': 'Lowerflood_depth',
+                                  'std_deviation_avg_flooding (ft)': 'std_flood_depth',
+                                  'avg_percentflooded (%)': 'p_time_flooded',
+                                  'Tide_Amp (ft)': 'Tide_Amp_ft'})
+
+
+# Formula for Bayesian Linear Regression (follows R formula syntax
+# PyMC3 for Bayesian Inference
+import pymc3 as pm
+formula = 'AccretionRate ~ ' + ' + '.join(dfwhole.columns[1:])
+# Context for the model
+with pm.Model() as normal_model:
+    # The prior for the model parameters will be a normal distribution
+    family = pm.glm.families.Normal()
+
+    # Creating the model requires a formula and data (and optionally a family)
+    pm.GLM.from_formula(formula, data=dfwhole, family=family)
+
+    # Perform Markov Chain Monte Carlo sampling
+    normal_trace = pm.sample(draws=2000, chains=2, tune=500)
+
+# Investigate the posterior of the weight parameters (per feature)
+
+# Investigate teh posterior of the weight parameters for each marsh type
+
+# Use the MAP estimate of the weights to define weight values then predict
 
 
 
